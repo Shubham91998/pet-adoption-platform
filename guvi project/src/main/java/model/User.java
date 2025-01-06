e2a4406ea9107f7a01950b5bdf90a0e0d4066ad6
23SCSE1011146 <ansh.dwivedi220@gmail.com>
2025-01-06 17:11:10 +0530

    Add files via upload

---
+++ guvi project/src/main/java/model/User.java
@@ -0,0 +1,63 @@
+package model;
+
+public class User {
+    private String name;
+    private String address;
+    private String email;
+    private String mobile;
+    private String password;
+    private Role role;
+
+    public enum Role {
+        USER, ADMIN
+    }
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
